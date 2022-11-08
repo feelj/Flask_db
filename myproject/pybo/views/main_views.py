536a833f6ee0c36d1e279a9b0a6a4a2a79bc0f41
feelj <rkdska1024@nate.com>
2022-11-08 17:31:18 +0900

    asd

--- myproject/pybo/views/main_views.py
+++ myproject/pybo/views/main_views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, url_for
 from werkzeug.utils import redirect
 
-# from pybo.models import Question
+from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -15,7 +15,7 @@ def hello_pybo():
 def index():
     return redirect(url_for('question._list'))
 
-# @bp.route('/detail/<int:question_id>/')
-# def detail(question_id):
-#     question = Question.query.get_or_404(question_id)
-#     return render_template('question/question_detail.html', question=question)
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get_or_404(question_id)
+    return render_template('question/question_detail.html', question=question)
